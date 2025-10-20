a0e3536c202792810635fffa80ae44c37dde56c7
user <shd36636@gmail.com>
2025-10-21 01:06:31 +0500

    add http

--- backend/src/main.py
+++ backend/src/main.py
@@ -35,7 +35,8 @@ async def health_check():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["localhost:3000", "http://localhost:3000"],
+    allow_origins=["localhost:3000", "http://localhost:3000", "http://95.182.118.118"],
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
